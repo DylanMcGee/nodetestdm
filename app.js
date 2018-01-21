fea8043049a1d8aaf6806d39cd730b99ece2c934
Dylan <dylanmcgee@outlook.com>
2018-01-21 11:15:57 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-console.log('hello heroku');
-console.log(2+2);
+var express = require('express');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.set('views', __dirname + '/views')
+
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+	res.render('home');
+})
+
+app.listen(app.get('port'), function(){
+	console.log('Node app is running on port ', app.get('port'));
+});
