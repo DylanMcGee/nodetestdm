47fbdb733677a15bdc1ef1dbde698727c947ccb4
Dylan <dylanmcgee@outlook.com>
2018-01-21 12:10:22 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var express = require('express');
-
+var mysql = require('mysql');
+mysql://b12b979bd038a3:67495600@us-cdbr-iron-east-05.cleardb.net/heroku_22e1c66f1166b4b?reconnect=true
+var connection = mysql.createConnection({
+	host: 'us-cdbr-iron-east-05.cleardb.net',
+	username: 'b12b979bd038a3',
+	password: '67495600',
+	database: 'heroku_22e1c66f1166b4b'
+});
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,9 +16,19 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res){
-	res.render('home');
+	var sql = 'SELECT * FROM heroku_22e1c66f1166b4b.testdbheroku';
+	connection.query(sql, function(err, result, fields){
+		if(err){
+			console.log(err);
+		}else{
+			res.render('home', {
+				dbinfo : result
+			});
+		}
+	})
 })
 
 app.listen(app.get('port'), function(){
 	console.log('Node app is running on port ', app.get('port'));
 });
+
